14fa918567a92a378f558593a789f4a181ac53cc
Kang <kang.45@foxmail.com>
2023-09-08 12:33:09 +0800

    包路径更改

--- src/main/java/com/kang/test/Test.java
+++ src/main/java/com/kang/test/Test.java
@@ -16,19 +16,49 @@ import cn.hutool.crypto.Mode;
 import cn.hutool.crypto.Padding;
 import org.apache.commons.codec.binary.Base64;
 
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.util.Iterator;
+import java.util.Properties;
+
 
 public class Test {
-    public static void main(String[] args){
-        AesEntity aes_entity = new AesEntity();
-        aes_entity.setMode(Mode.CBC);
-        aes_entity.setPadding(Padding.NoPadding);
-        aes_entity.setPlainText("1231231231231234");
-        aes_entity.setKey("123");
-        aes_entity.setCharset("gb2312");
-        aes_entity.setDataBlock(16);
-        aes_entity.setIv("1231231231231234");
+//    public static void main(String[] args){
+//        AesEntity aes_entity = new AesEntity();
+//        aes_entity.setMode(Mode.CBC);
+//        aes_entity.setPadding(Padding.NoPadding);
+//        aes_entity.setPlainText("1231231231231234");
+//        aes_entity.setKey("123");
+//        aes_entity.setCharset("gb2312");
+//        aes_entity.setDataBlock(16);
+//        aes_entity.setIv("1231231231231234");
+//
+//        AesEncryptImpl aes_encrypt = new AesEncryptImpl(aes_entity);
+//        System.out.println(new String(Base64.encodeBase64(aes_encrypt.AES_Encrypt_all())));
+//    }
+public static void main(String[] args) {
+    Properties prop = new Properties();
+    try{
+        //读取属性文件a.properties
+        InputStream in = new BufferedInputStream(new FileInputStream("config.properties"));
+        prop.load(in);     ///加载属性列表
+        Iterator<String> it=prop.stringPropertyNames().iterator();
+        while(it.hasNext()){
+            String key=it.next();
+            System.out.println(key+":"+prop.getProperty(key));
+        }
+        in.close();
 
-        AesEncryptImpl aes_encrypt = new AesEncryptImpl(aes_entity);
-        System.out.println(new String(Base64.encodeBase64(aes_encrypt.AES_Encrypt_all())));
+        ///保存属性到b.properties文件
+        FileOutputStream oFile = new FileOutputStream("config.properties", true);//true表示追加打开
+        prop.setProperty("phone", "10086");
+        prop.store(oFile, "The New properties file");
+        oFile.close();
     }
+    catch(Exception e){
+        System.out.println(e);
+    }
+}
 }
